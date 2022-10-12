4d2f0db8d8e900a987c52f3a6dd6540de2039e0f
ADHIL-MOHAMMED-P-N <adhilmohammedpn@gmail.com>
2022-10-12 20:36:59 +0530

    bootstrap added

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const path = require('path')
 const app = express();
 port = 3000;
-
+app.use(express.static('public'));
 app.get('/',(req,res)=>{
-    res.send("Blogpad")
+    res.sendFile(path.resolve(__dirname,'pages/index.html'))
 })
 
 app.listen(3000,()=>console.log(`app listening to the port ${port}`))
