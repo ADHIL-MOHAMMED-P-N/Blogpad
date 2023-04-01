899ef8521006630f2bb2406eaa1736ffc8bb6055
ADHIL-MOHAMMED-P-N <adhilmohammedpn@gmail.com>
2023-04-01 11:20:02 +0530

    Error handler,asynch-handler:controllers

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddleware");
 const path = require("path");
 
 port = process.env.PORT || 5000;
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.use("/api/blogs", require("./routes/blogRoutes"));
+
+app.use(errorHandler); //down the routes
+
 app.listen(port, () => console.log(`app listening to the port ${port}`));
--- controllers/blogControllers.js
+++ controllers/blogControllers.js
@@ -1,30 +1,36 @@
+const asyncHandler = require("express-async-handler"); /* handling exceptions inside of async express routes and passing them to your express error handlers.No need to write try-catch for every async routes */
+
 //Get blogs
 //GET /api/goals
 //Private
-const getBlogs = (req, res) => {
+const getBlogs = asyncHandler(async (req, res) => {
   res.status(200).json({ Message: "Read blogs" });
-};
+});
 
-//Create blogs
+//Set blogs
 //POST /api/goals
 //Private
-const setBlogs = (req, res) => {
+const setBlogs = asyncHandler(async (req, res) => {
+  if (!req.body.text) {
+    res.status(400);
+    throw new Error("Please add a text field");
+  }
   res.status(200).json({ Message: "Create Blogs" });
-};
+});
 
 //Update blogs
 //PUT /api/goals/:id
 //Private
-const updateBlogs = (req, res) => {
+const updateBlogs = asyncHandler(async (req, res) => {
   res.status(200).json({ Message: `Update ${req.params.id} Blogs` });
-};
+});
 
 //Delete blogs
 //DELETE /api/goals/id
 //Private
-const deleteBlogs = (req, res) => {
+const deleteBlogs = asyncHandler(async (req, res) => {
   res.status(200).json({ Message: `Delete ${req.params.id} Blogs` });
-};
+});
 
 module.exports = {
   getBlogs,
